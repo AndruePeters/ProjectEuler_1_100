8018c6123a24b36a3f1a76481f08899242d13014
root <5511409+AndruePeters@users.noreply.github.com>
2020-02-17 20:25:17 -0500

    Added fib_sum methods

--- src/problems/p2/src/p2_main.cpp
+++ src/problems/p2/src/p2_main.cpp
@@ -7,13 +7,9 @@
 
 int main()
 {
-     std::vector<int> fibNumbers(30);
-     std::iota(fibNumbers.begin(), fibNumbers.end(), 1);
-
-     for (auto num : fibNumbers) {
-         std::cout << "Fibonacci of " << num << ": " << fib_fast_matrix_exp(num) << "\n";
-     }
-
-
+    const unsigned fib_limit = 4'000'000;
+    std::cout << "fib_sum_memoization: " << fib_sum_memoization(fib_limit) << std::endl;
+    std::cout << "fib_sum_eigen_matrix: " << fib_sum_eigen_matrix(fib_limit) << std::endl;
+    std::cout << "fib_sum_golden_ratio: " << fib_sum_golden_ratio(fib_limit) << std::endl;
     return 0;
 }
