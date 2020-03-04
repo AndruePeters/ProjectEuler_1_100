daf3001c10ad7e3f3c82f285b5c578fe77d1afa5
root <5511409+AndruePeters@users.noreply.github.com>
2020-03-03 19:45:26 -0500

    Support for p3 benchmarks

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,8 @@
 #include <benchmark/benchmark.h>
+
+
 #include "problems/p1/include/p1_benchmark.h"
 #include "problems/p2/include/p2_benchmark.h"
+#include "problems/p3/include/p3_benchmark.h"
 
 BENCHMARK_MAIN();
