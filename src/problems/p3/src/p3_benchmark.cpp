32fe7837613c3f80ae56e3f221edd1a8ae41ed33
root <5511409+AndruePeters@users.noreply.github.com>
2020-03-18 15:00:42 -0400

    Added benchmarks for p3

--- src/problems/p3/src/p3_benchmark.cpp
+++ src/problems/p3/src/p3_benchmark.cpp
@@ -1,10 +1,28 @@
-#ifndef P3_BENCHMARK_H
-#define P3_BENCHMARK_H
-
 #include <benchmark/benchmark.h>
-#include "p3.h"
+#include <p3.h>
 
+#define CUSTOM_RANGE_P3 Range(100, 1000000)
 
+static void BM3_primegen_eratosthenes(benchmark::State& state)
+{
+    const unsigned limit = state.range(0);
+    for (auto _ : state) {
+        auto primes = primeSieveofEratosthenes(limit);
+    }
+} BENCHMARK(BM3_primegen_eratosthenes)->CUSTOM_RANGE_P3;
 
+static void BM3_primegen_sundaram(benchmark::State& state)
+{
+    const unsigned limit = state.range(0);
+    for (auto _ : state) {
+        auto primes = primeSieveOfSundaram(limit);
+    }
+} BENCHMARK(BM3_primegen_sundaram)->CUSTOM_RANGE_P3;
 
-#endif
+static void BM3_primegen_atkin(benchmark::State& state)
+{
+    const unsigned limit = state.range(0);
+    for (auto _ : state) {
+        auto primes = primeSieveOfAtkin(limit);
+    }
+} BENCHMARK(BM3_primegen_atkin)->CUSTOM_RANGE_P3;
