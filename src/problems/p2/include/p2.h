8abc3f0ed239c993c1719043fa478a1302b752c9
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-03-25 22:51:45 -0400

    Refactored to use math_lib

---
+++ src/problems/p2/include/p2.h
@@ -0,0 +1,17 @@
+///
+/// \author Andrue Peters
+/// \date 3/25/20
+///
+/// \brief Solves ProjectEuler Problem 2
+///
+
+#ifndef P2_H_
+#define P2_H_
+
+///
+/// \brief Returns the sum of all even fibonacci numbers
+/// Makes use of the realization that every 3rd fibonacci number is even
+///
+unsigned even_fibonacci_sum(const unsigned limit);
+
+#endif
