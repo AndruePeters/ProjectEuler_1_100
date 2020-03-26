20426928fd5a910c96f6130fb2dff78740315eb6
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-03-25 22:50:41 -0400

    Only shows correct answer

--- src/problems/p3/src/p3_main.cpp
+++ src/problems/p3/src/p3_main.cpp
@@ -1,23 +1,17 @@
-#include <p3.h>
-#include <spdlog/spdlog.h>
+///
+/// \author Andrue Peters
+/// \date 3/25/20
+///
+/// \brief Solves ProjectEuler Problem 3
+///
 
-#include <algorithm>
 #include <iostream>
-#include <random>
-#include <set>
 
-#include <boost/container/flat_map.hpp>
+#include <p3.h>
 
 int main()
 {
-    unsigned long factorize = 600851475143;
-    auto factorMap = primeFactorization1(factorize);
-
-    const auto greatestFactor = std::max_element(factorMap.begin(), factorMap.end(), 
-    [](const std::pair<unsigned, unsigned>& p1, const std::pair<unsigned, unsigned>& p2) {
-        return p1.first > p2.second;
-    });
-
-    std::cout << "Greatest factor: " << greatestFactor->first << " appeared " << greatestFactor->second << " times.\n";
+    const unsigned long factorize = 600851475143;
+    std::cout << "Greatest factor: " << largest_prime_factor(factorize) << '\n';
     return 0;
 }
