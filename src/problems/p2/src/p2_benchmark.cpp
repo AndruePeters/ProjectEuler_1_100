a5be4d744b3b30ae0131155a186d590af14b0a4c
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-03-25 22:48:37 -0400

    Refactored to only test the Project Euler solution

--- src/problems/p2/src/p2_benchmark.cpp
+++ src/problems/p2/src/p2_benchmark.cpp
@@ -1,95 +1,21 @@
-#include <benchmark/benchmark.h>
+///
+/// \author Andrue Peters
+/// \date 3/25/20
+///
+
 #include <p2.h>
 
-#define CUSTOM_RANGE_P2 Range(0, 30)
+// include Google Benchmark
+#include <benchmark/benchmark.h>
 
-
-static void BM2_fib_recursive(benchmark::State& state)
+///
+/// Benchmark for Problem 2 of Project Euler
+///
+static void Problem2(benchmark::State& state)
 {
-    const unsigned fib = state.range(0);
+    const unsigned fib_limit = 4'000'000;
     for (auto _ : state) {
-        fib_recursive(fib);
+        even_fibonacci_sum(fib_limit);
     }
-} BENCHMARK(BM2_fib_recursive)->CUSTOM_RANGE_P2;
+} BENCHMARK(Problem2);
 
-static void BM2_fib_memoization(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_memoization(fib);
-    }
-} BENCHMARK(BM2_fib_memoization)->Range(8, 32000);
-
-static void BM2_fib_memoization_revIf(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_memoization_revIf(fib);
-    }
-} BENCHMARK(BM2_fib_memoization_revIf)->Range(8, 32000);
-
-static void BM2_fib_loop(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_loop(fib);
-    }
-} BENCHMARK(BM2_fib_loop)->Range(8, 32000);
-
-static void BM2_fib_golden_ratio(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_golden_ratio(fib);
-    }
-} BENCHMARK(BM2_fib_golden_ratio)->Range(8, 32000);
-
-static void BM2_fib_dynamic(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_dynamic(fib);
-    }
-} BENCHMARK(BM2_fib_dynamic)->Range(8, 32000);
-
-
-static void BM2_fib_eigen_matrix_exp(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_eigen_matrix_exp(fib);
-    }
-} BENCHMARK(BM2_fib_eigen_matrix_exp)->Range(8, 32000);
-
-static void BM2_fib_eigen_matrix_exp_fast(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_eigen_matrix_exp_fast(fib);
-    }
-} BENCHMARK(BM2_fib_eigen_matrix_exp_fast)->Range(8, 32000);
-
-
-static void BM2_fib_sum_memoization(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_sum_memoization(fib);
-    }
-} BENCHMARK(BM2_fib_sum_memoization)->Range(8, 32000);
-
-static void BM2_fib_sum_eigen_matrix(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_sum_eigen_matrix(fib);
-    }
-} BENCHMARK(BM2_fib_sum_eigen_matrix)->Range(8, 32000);
-
-static void BM2_fib_sum_golden_ratio(benchmark::State& state)
-{
-    const unsigned fib = state.range(0);
-    for (auto _ : state) {
-        fib_sum_golden_ratio(fib);
-    }
-} BENCHMARK(BM2_fib_sum_golden_ratio)->Range(8, 32000);
