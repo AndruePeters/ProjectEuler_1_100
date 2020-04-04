7f38da80fd6c31c747cf5b4a177143f9ba05f43a
root <5511409+AndruePeters@users.noreply.github.com>
2020-04-03 22:55:19 -0400

    Added GTest for problem 4

--- src/problems/p4/src/p4_test.cpp
+++ src/problems/p4/src/p4_test.cpp
@@ -6,3 +6,11 @@
 #include <p4.h>
 
 #include <gtest/gtest.h>
+
+TEST(ProjectEuler, Problem4)
+{
+    const unsigned ProjectEulerAnswer = 906'609;
+    const unsigned digit_length = 3;
+    ASSERT_EQ(ProjectEulerAnswer, largest_palindrome(digit_length));
+}
+
