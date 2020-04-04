c6a00039131ec9d70ee54e1048182882cb5b2953
root <5511409+AndruePeters@users.noreply.github.com>
2020-04-03 22:55:45 -0400

    Support for problem 4

--- src/problems/p4/src/p4_main.cpp
+++ src/problems/p4/src/p4_main.cpp
@@ -2,7 +2,7 @@
 /// \author Andrue Peters
 /// \date 4/2/20
 ///
-/// \brief Solves ProjectEuler Problem 3
+/// \brief Solves ProjectEuler Problem 4
 ///
 
 #include <iostream>
@@ -11,5 +11,7 @@
 
 int main()
 {
+    std::cout << "Largest palindrome product of two 3 digit numbers: " 
+              << largest_3digit_palindrome_product() << '\n';
     return 0;
 }
